dd8d2dd8abd6a21c75508c5ca19cd11e2769cbc4
Lunar 7 sep <sophie3y@gmail.com>
2025-09-21 22:44:37 +0200

    Username and password

---
+++ UserAccessCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine() ?? "";
+
+Console.Write("Enter password: ");
+string password = Console.ReadLine() ?? "";
+
+bool usernameValid = username.Length >= 3;   // bruger .Length
+bool passwordValid = password.Contains('s'); // bruger .Contains('s')
+
+if (usernameValid && passwordValid)
+{
+    Console.WriteLine("Both username and password are valid.");
+}
+else
+{
+    Console.WriteLine("Username or password is not valid.");
+}
